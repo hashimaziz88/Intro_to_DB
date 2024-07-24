9b7f5d06aea5c2e9d64c0967b477a6541182719a
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-07-24 20:46:20 +0200

    update task_6

--- task_6.sql
+++ task_6.sql
@@ -1,4 +1,4 @@
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES 
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
