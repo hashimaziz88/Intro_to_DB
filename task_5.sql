15ea76a6e534173da8ca16a969eea186d03ec2f8
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-07-24 20:45:18 +0200

    update task5

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,4 @@
-INSERT INTO Customers (customer_id, customer_name, email, address)
+USE alx_book_store;
+
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
